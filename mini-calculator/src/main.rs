2eef7b7dda53380681e927a88fa9927e02eb9b35
Anurag Gaikwad <gaikwadanurag16@gmail.com>
2025-08-14 22:29:30 +0530

    Mini-calculator updated

--- mini-calculator/src/main.rs
+++ mini-calculator/src/main.rs
@@ -3,11 +3,20 @@ use std::env;
 
 fn main() {
     
-    let operation  = env::args().nth(1).expect("Missing operation as input");
+    let operation  = env::args().nth(1).unwrap();
 
 
-    let a: f32 = env::args().nth(2).expect("No number given as input").parse().expect("not a valid number");
-    let b: f32 = env::args().nth(3).expect("No number given as input").parse().expect("not a valid number");
+    //.expect("msg here") can be used as substitute to .unwrap()
+    //.expect() carries the error msg that is shown to user if any error occurs.
+    //.unwrap() is just like if if is success then show the output; if unsuccessful then just crash the program.
+    //.expect() is good to use as it shows the error msg and does'nt crash the program.
+    
+    let a: f32 = env::args().nth(2).expect("Enter an integer").parse().expect("Not a valid number");
+    //let a: f32 = env::args().nth(2).unwrap().parse().unwrap();
+
+    let b: f32 = env::args().nth(3).expect("Enter an integer").parse().expect("Not a valid number");
+    //let b: f32 = env::args().nth(3).unwrap().parse().unwrap();
+
 
     let result = match operation.as_str() {
         "add" => a + b,
